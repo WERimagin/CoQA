752d4d04b48d9ad0b9698dd266f1493b4dae47e4
笹沢裕一 <sasazawa@sasazawayuuichinoMacBook-Pro.local>
2018-11-14 21:05:22 +0900

    add

--- QuestionMaker/model/seq2seq.py
+++ QuestionMaker/model/seq2seq.py
@@ -9,10 +9,10 @@ from QuestionMaker.model import Encoder,Decoder
 
 class Seq2Seq(nn.Module):
     def __init__(self,args):
-        super(Seq2seq, self).__init__()
+        super(Seq2Seq, self).__init__()
         self.encoder=Encoder(args)
         self.decoder=Decoder(args)
-        
+
     def forward(self, context_words,train=True):
         encoder_outputs, encoder_hidden = self.encoder(context_words)
         output = self.decoder(encoder_outputs,encoder_hidden)
