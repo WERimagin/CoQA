542693d4e30ccd0714997d09de2e700ce91b63e9
笹沢裕一 <sasazawa@sasazawayuuichinoMacBook-Pro.local>
2018-11-15 16:09:43 +0900

    add

--- question_maker/model/attention.py
+++ question_maker/model/attention.py
@@ -11,7 +11,9 @@ class Attention(nn.Module):
         self.attention_wight=nn.Linear(self.hidden_size,self.hidden_size*2)
 
     def forward(self,input,encoder_output):#input:(batch,hidden_size),encoder_input:(batch,seq_len,hidden_size*2)
-        #input*W*encoder_input:(batch,seq_len)(batch,hidden_size)*(W)*(batch,seq_len,hidden_size*2)
-        attention_output=F.softmax(torch.bmm(self.attention_wight(input),encoder_output),dim=-1)#(batch,seq_len)
-        attention_output=torch.bmm(attention_output,encoder_output)#(batch,hidden_size*2)
+        #input*W*encoder_input:(batch,seq_len)(batch,1,hidden_size)*(hidden_size,hidden_size*2)*(batch,seq_len,hidden_size*2)
+        attention_input=torch.unsqueeze(input,1)#(batch,1,hidden_size)
+        attention_encoder_input=torch.transpose(encoder_output,1,2)#(batch,hidden_size*2,seq_len)
+        attention_output=F.softmax(torch.bmm(self.attention_wight(attention_input),attention_encoder_input),dim=-1)#(batch,1,seq_len)
+        attention_output=torch.squeeze(torch.bmm(attention_output,encoder_output),1)#(batch,hidden_size*2)
         return attention_output
