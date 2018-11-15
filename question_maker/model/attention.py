10101bfd46877771814b86d72915e9eb10a19ab6
笹沢裕一 <sasazawa@sasazawayuuichinoMacBook-Pro.local>
2018-11-15 15:30:42 +0900

    add

---
+++ question_maker/model/attention.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Attention(nn.Module):
+
+    def __init__(self, args):
+        super(Attention, self).__init__()
+
+        self.attention_wight=nn.Linear(self.hidden_size,self.hidden_size*2)
+
+    def forward(self,input,encoder_output):#input:(batch,hidden_size),encoder_input:(batch,seq_len,hidden_size*2)
+        #input*W*encoder_input:(batch,seq_len)(batch,hidden_size)*(W)*(batch,seq_len,hidden_size*2)
+        attention_output=F.softmax(torch.bmm(self.attention_wight(input),encoder_output),dim=-1)#(batch,seq_len)
+        attention_output=torch.bmm(attention_output,encoder_output)#(batch,hidden_size*2)
+        return attention_output
--- question_maker/model/decoder.py
+++ question_maker/model/decoder.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from func.utils import Word2Id,DataLoader,make_vec,make_vec_c,to_var
+from from question_maker.model.decoder import Attention
 
 class Decoder(nn.Module):
 
@@ -16,19 +17,25 @@ class Decoder(nn.Module):
 
         self.word_embed=nn.Embedding(self.vocab_size, self.embed_size,padding_idx=0,
                                     _weight=torch.from_numpy(args.pretrained_weight).float())
-        self.gru=nn.GRU(self.embed_size,self.hidden_size,bidirectional=True,dropout=args.dropout,batch_first=True)
-        self.out=nn.Linear(self.hidden_size*2,self.vocab_size)
+        self.gru=nn.GRU(self.embed_size,self.hidden_size,bidirectional=False,dropout=args.dropout,batch_first=True)#decoderは双方向にできない
 
 
-    def decode_step(self,input):#(batch,1)
+        self.attention=Attention(args)
+        self.attention_wight=nn.Linear(self.hidden_size*3,self.hidden_size*3)
+        self.out=nn.Linear(self.hidden_size*3,self.vocab_size)
+
+    def decode_step(self,input,encoder_output):#(batch,1)
         embed=self.word_embed(input)#(batch,1,embed_size)
-        output,hidden=self.gru(embed,self.hidden)#(batch,1,hidden_size*2)
+        output,hidden=self.gru(embed,self.hidden)#(batch,1,hidden_size)
         self.hidden=hidden
-        output=torch.squeeze(self.out(output),1)#(batch,vocab_size)
+        output=torch.squeeze(1)#(batch,hidden_size)
+        attention_output=self.attention(output,encoder_output)#(batch,hidden_size*2)
+
+        output=self.out(F.relu(self.attention(torch.cat((output,attention_output),-1)))#(batch,vocab_size)
         predict=torch.argmax(output,dim=-1)#(batch)
         return output,predict
 
-    def forward(self,input,encoder_hidden,q_words):#input:(batch,seq_len),encoder_hidden:(seq_len,hidden_size*2),q_words:(batch,q_seq_len)
+    def forward(self,encoder_output,encoder_hidden,q_words):#input:(batch,seq_len),encoder_hidden:(seq_len,hidden_size*2),q_words:(batch,q_seq_len)
         batch_size=q_words.size(0)
         q_seq_len=q_words.size(1)
         self.hidden=encoder_hidden
@@ -36,7 +43,7 @@ class Decoder(nn.Module):
         current_input=to_var(torch.from_numpy(np.zeros((batch_size,1),dtype="long")))#最初の隠れベクトル,<SOS>
 
         for i in range(q_seq_len):#(batch,1)
-            output,predict=self.decode_step(current_input)#(batch,vocab_size)(batch)
+            output,predict=self.decode_step(current_input,encoder_output)#(batch,vocab_size)(batch)
             current_input=predict.view(-1,1)
             outputs[i]=output
 
