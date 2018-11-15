1b62552e51717debc2ed4c80702bb4fcc763b5d1
笹沢裕一 <sasazawa@sasazawayuuichinoMacBook-Pro.local>
2018-11-15 22:13:15 +0900

    add

--- question_maker/train.py
+++ question_maker/train.py
@@ -134,6 +134,7 @@ def model_handler(args,data,train=True):
                     f.write("epoch,{}\tbatch\t{}\tloss:{}\ttime:{}\n".format(epoch,i_batch,loss.data,now-start))
         else:
             predict_rate+=predict_calc(predict,q_words)/predict.size(1)
+            print(data_size,predict.size(1))
 
     with open("log.txt","a")as f:
         if train:
