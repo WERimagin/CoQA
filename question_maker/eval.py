20c2154d6801643dd0cb02ca4dc9ac8a2d566ea0
笹沢裕一 <sasazawa@sasazawayuuichinoMacBook-Pro.local>
2018-11-21 18:30:32 +0900

    add

--- question_maker/eval.py
+++ question_maker/eval.py
@@ -61,9 +61,6 @@ def data_loader(args,path,first=True):
 
     id2word={i:w for w,i in word2id.items()}
 
-
-
-
     data={"contexts_id":contexts_id,
         "questions_id":questions_id,
         "answers_id":answers_id,
